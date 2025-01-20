a93770e13222af8cfea54fa35f7cf6e3857b1ab9
Asmaa Hariti <haritiasmaa74@gmail.com>
2025-01-20 14:03:54 +0100

    making Case entity with hibernate validations and Lombok annotations

---
+++ src/main/java/ma/hariti/asmaa/wrm/inventivit/entity/Case.java
@@ -0,0 +1,45 @@
+package ma.hariti.asmaa.wrm.inventivit.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cases")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Case {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long caseId;
+
+    @NotNull(message = "Creation date cannot be null")
+    @CreationTimestamp
+    @Column(name = "creation_date", nullable = false, updatable = false)
+    private LocalDateTime creationDate;
+
+    @UpdateTimestamp
+    @Column(name = "last_update_date")
+    private LocalDateTime lastUpdateDate;
+
+    @NotBlank(message = "Title cannot be blank")
+    @Size(max = 255, message = "Title cannot be longer than 255 characters")
+    @Column(nullable = false)
+    private String title;
+
+    @NotBlank(message = "Description cannot be blank")
+    @Size(max = 2056, message = "Description cannot be longer than 2056 characters")
+    @Column(length = 2056, nullable = false)
+    private String description;
+}
