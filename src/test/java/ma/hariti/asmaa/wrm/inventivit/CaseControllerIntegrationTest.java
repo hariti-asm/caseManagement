59f824c4fc832ad3b46fd4e80d1b2f0549183bf6
Asmaa Hariti <haritiasmaa74@gmail.com>
2025-01-21 09:59:44 +0100

    adding application-test.yml to the integration test

--- src/test/java/ma/hariti/asmaa/wrm/inventivit/CaseControllerIntegrationTest.java
+++ src/test/java/ma/hariti/asmaa/wrm/inventivit/CaseControllerIntegrationTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.yaml")
 class CaseControllerIntegrationTest {
 
     @Autowired
